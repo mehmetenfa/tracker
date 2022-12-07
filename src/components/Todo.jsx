bb5fa3306bfaa9bd19a8bb00ca9be506bd87f60d
qxan <mehmet.enfa@gmail.com>
2022-12-07 13:02:29 +0300

    icons and todo

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import {AiFillDelete} from 'react-icons/ai'
+import {FiEdit} from 'react-icons/fi'
 
-const Todo = () => {
+const Todo = ({inputs, deleteItem}) => {
   return (
-    <div>Todo</div>
+    <div>
+        <h2>Todos</h2>
+        <div>
+            <button className="border px-2 m-2" >All</button>
+            <button className="border px-2 m-2" >Done</button>
+            <button className="border px-2 m-2" >Todo</button>
+        </div>
+        <div>
+            {inputs.map((each) => {
+                const {id, todo} = each
+                return (
+                    <div className="flex justify-between">
+                        <p>{todo}</p>
+                        <div>
+                            <FiEdit />
+                            <AiFillDelete />
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
+    </div>
   )
 }
 
