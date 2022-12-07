7d68921d3bf33d2b72d261e5caa5cb971a07511b
qxan <mehmet.enfa@gmail.com>
2022-12-07 12:54:41 +0300

    home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,26 @@
-import React, { useEffect, useState } from 'react'
-import Header from './Header'
-import Todo from './Todo'
+import React, { useEffect, useState } from "react";
+import Header from "./Header";
+import Todo from "./Todo";
 
 const Home = () => {
-    const [inputs, setInputs] = useState("")
+  const [inputs, setInputs] = useState(
+    JSON.parse(localStorage.getItem("todos")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(inputs));
+  }, [inputs]);
+
+  const deletItem = (id) => {
+        setInputs(inputs.filter((el) => el.id !== id))
+  }
+
   return (
     <div>
-        <Header />
-        <Todo />
+      <Header inputs={inputs} setInputs={setInputs} />
+      <Todo  inputs={inputs} deletItem={deletItem} />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
