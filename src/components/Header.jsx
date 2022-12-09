69ea1ccb92c148f017ebb7849bb2bccd13421bbb
qxan <mehmet.enfa@gmail.com>
2022-12-09 12:00:22 +0300

    todo update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,7 @@
-import { useState } from "react";
-const Header = ({ inputs, setInputs }) => {
-  const [todo, setTodo] = useState("");
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const id = new Date().getTime();
-    const newTodo = { id: id, todo: todo, isDone: false };
-    setInputs([...inputs, newTodo]);
-    setTodo("");
-  };
+const Header = () => {
   return (
-    <div className="flex flex-col items-center justify-center mt-11">
-      <h2 className="text-2xl">React To-Do List</h2>
-      <form className="mt-3 flex flex-col" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="todo"
-          value={todo}
-          className="border w-[13rem] h-[2rem] outline-none text-black"
-          placeholder="Enter To do"
-          onChange={(e) => setTodo(e.target.value)}
-        />
-        <input
-          type="submit"
-          value="Add"
-          className="border w-[13rem] h-[2rem] bg-slate-400 text-white mt-4"
-        />
-      </form>
+    <div className="">
+      
     </div>
   );
 };
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,42 +1,69 @@
-import {AiFillDelete} from "react-icons/ai";
-import {FiEdit} from "react-icons/fi";
+import React, { useState } from "react";
+import { RiDeleteBin3Fill, RiEdit2Fill } from "react-icons/ri";
+const App = () => {
+  const [todo, setTodo] = useState([
+    {
+      id: 0,
+      title: "Deneme Todo",
+      complated: false,
+    },
+  ]);
+  const [inputValue, setInputValue] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTodo = {
+      id: new Date(),
+      title: inputValue,
+      complated: false,
+    };
+    setTodo([...todo, newTodo]);
+    setInputValue("");
+  };
 
-const Todo = ({inputs, deleteItem}) => {
+  const handleDelete = (data) => {
+    setTodo(todo.filter((items) => items.id !== data.id));
+  };
 
-  const handleClick = (each) => {
-    console.log(each);
+  const handleUpdate = (idx) => {
+    console.log(idx)
   }
 
   return (
-    <div className="flex flex-col justify-center items-center mt-[3rem]">
-      <h2>To Do's</h2>
-      <div>
-        <button className=" bg-blue-400 rounded-2xl  w-[4rem] h-[2rem] text-white border px-2 m-21">
-          All
-        </button>
-        <button className="bg-green-400 rounded-2xl  w-[4rem] h-[2rem] text-white border px-2 m-2">
-          Done
-        </button>
-        <button className="bg-red-400  rounded-2xl w-[4rem] h-[2rem] text-white border px-2 m-2">
-          Todo
-        </button>
-      </div>
-      <div className="bg-red-500 w-[20rem] h-[20rem] overflow-scroll  rounded-xl">
-        {inputs.map((each) => {
-          const {id, todo} = each;
-          return (
-            <div className="flex justify-between">
-              <p>{todo}</p>
-              <div>
-                <FiEdit onClick={() => handleClick(each)} />
-                <AiFillDelete onClick={() => deleteItem(id)} />
+    <div className="max-w-screen-sm mx-auto h--[12rem] mt-[4rem] flex justify-center ">
+      <div className="p-10 bg-gray-300 rounded-lg w-full">
+        <p className="text-center mb-7 text-2xl font-semibold">
+          Todo App Project
+        </p>
+        <form className="text-center" onSubmit={handleSubmit}>
+          <input
+            placeholder="Enter something"
+            required
+            className="text-center px-4 py-2 bg-slate-500 hover:bg-slate-400 rounded-lg overflow-hidden focus:bg-slate-400 duration-300 placeholder:text-white outline-none"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+        </form>
+        <div className="bg-slate-200 mt-5 w-full h-[20rem] p-5 space-y-5 overflow-y-auto">
+          {todo?.map((item, idx) => (
+            <div
+              key={idx}
+              className="border-b-[1px] border-black border-opacity-50 pb-2 flex justify-between items-center"
+            >
+              <p>{item.title}</p>
+              <div className="flex items-center gap-3">
+                <RiDeleteBin3Fill
+                  className="text-xl cursor-pointer"
+                  onClick={() => handleDelete(item)}
+                />
+                <RiEdit2Fill className="text-xl cursor-pointer" onSubmit={() => handleUpdate(idx)} />
               </div>
             </div>
-          );
-        })}
+          ))}
+        </div>
       </div>
     </div>
   );
 };
-export default Todo;
+
+export default App;
